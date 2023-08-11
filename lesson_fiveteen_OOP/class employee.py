350eda12aff3e134e358c49a89b1165ef407f140
Bunlde-of-His <137227239+Bunlde-of-His@users.noreply.github.com>
2023-08-12 00:29:19 +0300

    Add files via upload

---
+++ lesson_fiveteen_OOP/class employee.py
@@ -0,0 +1,40 @@
+class Emploee:
+    def __init__(self, name, salary_per_day):
+        self.name = name
+        self.salary_per_day = salary_per_day
+
+    def work(self):
+        self.notification = "I come to the office."
+        return self.notification
+    
+    def __str__(self):
+       return f'My Position is {self.name}!'
+
+
+class Recruiter(Emploee):
+    def work(self):
+        return 'I come to the office and start to hiring.'
+
+class Developer(Emploee):
+    def work(self):
+        return 'I come to the office and start to coding.'
+
+
+def salary_comparing(first_salary, second_salary):
+    if first_salary > second_salary:
+        return 'Recruiter earn more money!'
+    elif first_salary < second_salary:
+        return 'Developer earn more money!'
+    else:
+        return 'Both salaries are equal!'
+
+
+recruiter_1 = Recruiter('Recruiter', 15)
+developer_1 = Developer('Developer', 20)
+
+print(recruiter_1.work())
+print(recruiter_1.__str__())
+print(developer_1.work())
+print(developer_1.__str__())
+
+print(salary_comparing(recruiter_1.salary_per_day, developer_1.salary_per_day))
---
+++ lesson_fiveteen_OOP/human and planet classes.py
@@ -0,0 +1,63 @@
+import logging
+
+class LogCreation:
+    def __new__(cls, *args, **kwargs):
+        instance = super().__new__(cls)
+        logging.info(f"Exemplar of the Class is created: {cls.__name__}")
+        return instance
+
+class Human(LogCreation):
+    CURRENT_YEAR = 2023
+    def __init__(self, name, date_of_birth):
+        self.name = name
+        self.date_of_birth = date_of_birth
+    
+    def __str__(self):
+        return f"Human(name={self.name}, date_of_birth={self.date_of_birth})"
+    
+    def to_str(self):
+        return f"Name: {self.name}, Date of Birth: {self.date_of_birth}"
+    
+    def get_age(self):
+        age =  self.CURRENT_YEAR - self.date_of_birth
+        return age
+
+class Planet(LogCreation):
+    def __init__(self, name):
+        self.name = name
+        self.humans = []
+    
+    def __str__(self):
+        return f"Planet(name={self.name})"
+    
+    def to_str(self):
+        return f"Planet: {self.name}"
+    
+    def add_human(self, humans):
+        self.humans.extend(humans)
+    
+    def get_count(self):
+        return len(self.humans)
+
+
+logging.basicConfig(level=logging.INFO)
+
+humans = [
+    Human("John", 1985),
+    Human("Jane", 1990),
+    Human("Jack", 1978),
+    Human("Jill", 1996),
+]
+
+planet = Planet("Earth")
+planet.add_human(humans)
+
+print(planet.to_str())
+print(f"Humans on the planet: {planet.get_count()}")
+
+for human in humans:
+    print(human.to_str())
+    print(f"Age: {human.get_age()} years")
+
+
+
