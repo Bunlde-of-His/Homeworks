ee85308c5d1c87346df0a349aae399a23788c3d5
Bunlde-of-His <137227239+Bunlde-of-His@users.noreply.github.com>
2023-08-16 19:08:45 +0300

    Update class employee.py

--- lesson_fiveteen_OOP/class employee.py
+++ lesson_fiveteen_OOP/class employee.py
@@ -8,25 +8,26 @@ class Employee:
         return self.notification
     
     def __str__(self):
-       return f'My Position is {self.name}!'
+        return f'My Position is {self.name}!'
+    
+    def __gt__(self, other):
+        return self.salary_per_day > other.salary_per_day
+    
+    def __lt__(self, other):
+        return self.salary_per_day < other.salary_per_day
+    
+    def __eq__(self, other):
+        return self.salary_per_day == other.salary_per_day
 
 
 class Recruiter(Employee):
     def work(self):
         return 'I come to the office and start to hiring.'
 
+
 class Developer(Employee):
     def work(self):
         return 'I come to the office and start to coding.'
-
-
-def salary_comparing(first_salary, second_salary):
-    if first_salary > second_salary:
-        return 'Recruiter earn more money!'
-    elif first_salary < second_salary:
-        return 'Developer earn more money!'
-    else:
-        return 'Both salaries are equal!'
 
 
 recruiter_1 = Recruiter('Recruiter', 15)
@@ -37,4 +38,10 @@ print(str(recruiter_1))
 print(developer_1.work())
 print(str(developer_1))
 
-print(salary_comparing(recruiter_1.salary_per_day, developer_1.salary_per_day))
+if recruiter_1 > developer_1:
+    print('Recruiter earn more money!')
+elif recruiter_1 < developer_1:
+    print('Developer earn more money!')
+else:
+    print('Both salaries are equal!')
+
