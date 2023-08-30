c1df1103db42acd8d520a731e27ad89eb65e5101
Bunlde-of-His <137227239+Bunlde-of-His@users.noreply.github.com>
2023-08-31 00:20:31 +0300

    Update test_developer.py

--- nineteenth_lesson_OOP/tests/test_developer.py
+++ nineteenth_lesson_OOP/tests/test_developer.py
@@ -21,9 +21,18 @@ class TestDeveloper(unittest.TestCase):
             other_developer = Developer("Mallory", 200, ["Python"], "mallory@example.com")
             self.assertTrue(self.developer == other_developer)
 
+    def test_add_developers(self):
+        other_developer = Developer("Charlie", 150, ["Python", "Django"], "charlie@example.com")
+        combined_developer = self.developer + other_developer
+
+        expected_name = "Bob Charlie"
+        expected_tech_stack = set(["Python", "Django"])
+        expected_salary = max(self.developer.salary_per_day, other_developer.salary_per_day)
+
+        self.assertEqual(combined_developer.name, expected_name)
+        self.assertEqual(set(combined_developer.tech_stack), expected_tech_stack)
+        self.assertEqual(combined_developer.salary_per_day, expected_salary)
+
+
     if __name__ == '__main__':
         unittest.main()
-
-
-    # Add more test methods for other Developer methods and behaviors
-
